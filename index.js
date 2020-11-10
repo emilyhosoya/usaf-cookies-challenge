a4a0b7c824c8454848326abe140f4b421fd869b8
Emily Hosoya <emilyhosoya@gmail.com>
2020-11-10 16:05:29 -0700

    Fixed if statement

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var cookieParser = require('cookie-parser')
 var app = express()
 app.use(cookieParser())
 
-app.get('/hello', function(req, res) {
-    if(res.cookie('name'))
-    res.send(`Welcome ${req.cookies.name}!`)
+app.get('/hello', function(req, res) {    
+    if(req.cookies.name !== undefined) {  
+        res.send(`Welcome ${req.cookies.name}!`)
+    } else {
+        res.send('Looks like no cookie was set...')
+    }
 })
 
 app.get('/login', function(req, res) {
