5bf115eaf51c746d5f29b65a7fcf50d123a6405a
Emily Hosoya <emilyhosoya@gmail.com>
2020-11-12 08:23:15 -0700

    Changed login route to a POST using Katie's example

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-var express = require('express')
-var cookieParser = require('cookie-parser')
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const app = express()
 
-var app = express()
 app.use(cookieParser())
+app.use(bodyParser.json())
 
-app.get('/hello', function(req, res) {    
-    if(req.cookies.name !== undefined) {  
-        res.send(`Welcome ${req.cookies.name}!`)
-    } else {
-        res.send('Looks like no cookie was set...')
-    }
+app.get('/hello', (req, res) => {    
+    let name = req.cookies.name
+    name ?  
+    res.send(`Welcome ${name}!`) :
+    res.send(`You're not logged in`)
 })
 
-app.get('/login', function(req, res) {
-    res.cookie('name', 'Emily').send('Cookie is set!')
+app.post('/login', (req, res) => {
+    let user = req.body
+    let result
+
+    if (user.name) {
+        result = {
+            "status": "success",
+            "message": "You're logged in"
+        }
+    } else {
+        result = {
+            "status": "failure",
+            "message": "You have not logged in"
+        }
+        res.status(404).json(result)
+    } 
+
+    res.cookie('name', user.name)
+    res.json(result)
 })
 
 app.listen(8080)
